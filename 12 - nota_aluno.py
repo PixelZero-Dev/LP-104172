dbb0395e064fff107e8768d6895353508cdd8b37
Rafael Sales <128626953+PixelZero-Dev@users.noreply.github.com>
2026-08-27 16:08:25 -0300

    Add files via upload

--- 12 - nota_aluno.py
+++ 12 - nota_aluno.py
@@ -2,22 +2,21 @@ import os
 
 os.system('cls')
 
-nome = input('Digite seu nome: ')
-idade = int(input('Digite sua idade: '))
-primeira_nota = float(input('Digite a primeira nota: '))
-segunda_nota = float(input('Digite a segunda nota: '))
-terceira_nota = float(input('Digite a terceira nota: '))
+nome = input('Digite o nome do aluno: ')
+idade = int(input('Digite a idade do aluno: '))
+primeira_nota = float(input('Digite a primeira nota do aluno: '))
+segunda_nota = float(input('Digite a segunda nota do aluno: '))
+terceira_nota = float(input('Digite a terceira nota do aluno: '))
+
+print('Nome do aluno: ', nome)
+print('Idade do aluno: ', idade)
+print('Primeira nota do aluno: ', primeira_nota)
+print('Segunda nota do aluno: ', segunda_nota)
+print('Terceira nota do aluno: ', terceira_nota)
 
 media = (primeira_nota) + (segunda_nota) + (terceira_nota) / 3
 
 if media >= 7:
-    print('Aprovado.')
+    print('O Aluno está Aprovado. ')
 else:
-    print('Reprovado')
-
-
-print('Nome: ', nome)
-print('Idade: ', idade)
-print('Primeira nota: ', primeira_nota)
-print('Segunda nota: ', segunda_nota)
-print('Terceira nota: ', terceira_nota)
+    print('O Aluno está Reprovado ')
