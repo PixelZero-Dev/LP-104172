8ce283e904dec664c3079057930ead4a068fd281
Rafael Sales <128626953+PixelZero-Dev@users.noreply.github.com>
2026-08-28 16:36:57 -0300

    Add files via upload

--- 17 - maior_menor (segunda parte).py
+++ 17 - maior_menor (segunda parte).py
@@ -3,8 +3,8 @@ os.system('cls')
 
 numero_um = int(input('Digite um número: '))
 numero_dois = int(input('Digite outro número: '))
-max = numero_um, numero_dois
-min = numero_dois, numero_dois
+menor = max (numero_um, numero_dois)
+maior = min (numero_dois, numero_dois)
 
 print(f'\n Maior: {numero_um}')
 print(f'\n Menor: {numero_dois}')
